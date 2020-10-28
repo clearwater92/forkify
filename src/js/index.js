a275c2b094171e51e5381ea4a19a48277c4e5db3
clearwater <clearwater92@naver.com>
2020-10-29 01:25:55 +0900

    first API calls

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,12 @@
-import str from './models/Search';
-//import { add as a, multiply as m, ID } from './views/searchView';
-import * as searchView from './views/searchView';
+import axios from 'axios';
+async function getResults(query) {
+    try {
+        const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    } catch (error) {
+        alert(error);
+    }
+}
 
-console.log(`Using imported functions! ${searchView.add(searchView.ID, 403)} and ${searchView.multiply(7089, 5414)}, ${str}`);
+getResults('pizza');
