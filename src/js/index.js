82dae371ff3e365afbd7d80e011fabb5e75aa31e
clearwater <clearwater92@naver.com>
2020-10-28 23:57:30 +0900

    babel, webpack

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,5 @@
-// Global app controller
+import str from './models/Search';
+//import { add as a, multiply as m, ID } from './views/searchView';
+import * as searchView from './views/searchView';
+
+console.log(`Using imported functions! ${searchView.add(searchView.ID, 403)} and ${searchView.multiply(7089, 5414)}, ${str}`);
